466340944b07b299a17f6cf795765a09cbedd596
kpacha <kpacha666@gmail.com>
2016-04-14 21:38:44 +0200

    fix the haproxy parser tests for go1.6

--- parser/haproxy/haproxy_test.go
+++ parser/haproxy/haproxy_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func ExampleParseHAProxyStats() {
+func ExampleHAProxy() {
 	parser := NewHAProxy("testing-node")
 	content := newTestingReader(`# pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,comp_in,comp_out,comp_byp,comp_rsp,lastsess,last_chk,last_agt,qtime,ctime,rtime,ttime,
 http-in,FRONTEND,,,1,2,2000,12,8393,141134,0,0,0,,,,,OPEN,,,,,,,,,1,2,0,,,,0,1,0,2,,,,0,9,0,2,0,0,,1,1,12,,,0,0,0,0,,,,,,,,
